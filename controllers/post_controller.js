f5d1b98757eb4e47bd6a8414aeaa9d2a579449b7
Ayush Tarway <1ms20is127@msrit.edu>
2023-02-23 17:13:45 +0530

    Deleting post and comment

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,6 +1,9 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment')
 
-module.exports.create = function(req, res){
+
+module.exports.create = function(req, res)
+{
     Post.create({
         content: req.body.content,
         user: req.user._id
@@ -10,3 +13,22 @@ module.exports.create = function(req, res){
         return res.redirect('back');
     });
 }
+
+module.exports.destroy = function(request,response){
+    Post.findById(request.params.id,function(err,post)
+    {
+        // .id means converting the object id into string
+        if(post.user == request.user.id){
+            post.remove();
+
+            Comment.deleteMany({post:request.params.id},function(err){
+                return response.redirect('back');
+            })
+        }else{
+            return response.redirect('back');
+        }
+    })
+}
+
+
+
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,8 @@ const passport = require('passport');
 const postcontrolller = require('../controllers/post_controller');
 
 router.post('/create',passport.checkAuthentication,postcontrolller.create);
+router.get('/destroy/:id',passport.checkAuthentication,postcontrolller.destroy);
+
+ 
 
 module.exports = router;
