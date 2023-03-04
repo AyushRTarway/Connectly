39b4fe8ef9e0c1fec61df3d1438c5dbe40259636
Ayush Tarway <1ms20is127@msrit.edu>
2023-03-05 00:57:20 +0530

    Tried socket.io

--- config/chat_socket.js
+++ config/chat_socket.js
@@ -9,6 +9,20 @@ module.exports.chatSockets = function(socketServer){
 
 
     io.sockets.on('connection', function (socket) {
-        console.log("New connection received "+ socket.id);
+        console.log("New connection received " + socket.id);
+        
+        socket.on('disconnect', function () {
+            console.log('Socket Disconnected');
+        })
+
+        socket.on('join_room', function (data) {
+            console.log("joining the request rec.. ", data);
+            
+            socket.join(data.chatroom)
+
+            io.in(data.chatroom).emit('user_joined', data);
+        })
     })
+
+
 }
